e8e4f49fc83a4991358fa26c1e8d375a64d5c700
fjgalvan <fjgalvanx87@gmail.com>
2018-09-29 20:41:55 -0300

    Original.

---
+++ sugerenciasExternasPP2/sugerenciasExternasPP2.Principal/src/main/java/org/sugerenciasExternasPP2/Principal/App.java
@@ -0,0 +1,13 @@
+package org.sugerenciasExternasPP2.Principal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ sugerenciasExternasPP2/sugerenciasExternasPP2.vistas/src/main/java/org/sugerenciasExternasPP2/vistas/App.java
@@ -0,0 +1,13 @@
+package org.sugerenciasExternasPP2.vistas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
