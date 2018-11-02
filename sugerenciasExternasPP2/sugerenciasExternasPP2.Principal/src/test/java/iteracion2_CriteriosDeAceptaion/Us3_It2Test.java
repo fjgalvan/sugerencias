e9db368ec9101af23da27365f5845bdc20550f29
fjgalvan <fjgalvanx87@gmail.com>
2018-11-02 15:21:14 -0300

    Todos los criterios de aceptacion funcionan

--- sugerenciasExternasPP2/sugerenciasExternasPP2.Principal/src/test/java/iteracion2_CriteriosDeAceptaion/Us3_It2Test.java
+++ sugerenciasExternasPP2/sugerenciasExternasPP2.Principal/src/test/java/iteracion2_CriteriosDeAceptaion/Us3_It2Test.java
@@ -1,5 +1,90 @@
 package iteracion2_CriteriosDeAceptaion;
 
-public class Us3_It2Test {
+import static org.junit.Assert.assertEquals;
 
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import properties.Constants;
+
+public class Us3_It2Test {
+	Properties propiedadesEspanol;
+	Properties propiedadesIngles;
+	
+	@Before
+	public void init(){
+		propiedadesEspanol = new Properties();
+		try {
+			propiedadesEspanol.load(new FileReader(
+					Constants.ROUTE_ESPANOL));
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+
+		propiedadesIngles = new Properties();
+		try {
+			propiedadesIngles.load(new FileReader(
+					Constants.ROUTE_INGLES));
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+	}
+	
+	//Si se elige la opción de idioma español, entonces el TAG de comida chatarras es “chatarras”. 
+	@Test
+	public void test1(){
+		System.out.println("Test 1!");
+		String res="";
+		Enumeration<Object> keys = propiedadesEspanol.keys();
+
+		while (keys.hasMoreElements()) {
+			Object key = keys.nextElement();
+			if (key.equals("filtroChatarras")) {
+				res=propiedadesEspanol.get("filtroChatarras").toString();
+			}
+		}
+		System.out.println("res: "+res);
+		assertEquals(res, "1_Chatarras");
+	}
+	
+	//Si se elige la opción de idioma inglés, entonces el TAG de comida chatarras es “scrap”. 
+	@Test
+	public void test2(){
+		System.out.println("Test 2!");
+		String res="";
+		Enumeration<Object> keys = propiedadesIngles.keys();
+
+		while (keys.hasMoreElements()) {
+			Object key = keys.nextElement();
+			if (key.equals("filtroChatarras")) {
+				res=propiedadesIngles.get("filtroChatarras").toString();
+			}
+		}
+		System.out.println("res: "+res);
+		assertEquals(res, "1_Scrap");
+	}
+	
+	//Si se tiene la opción de ingresar bebidas en español, entonces queda “ ” 
+	@Test
+	public void test3(){
+		System.out.println("Test 3!");
+		String res="";
+		Enumeration<Object> keys = propiedadesEspanol.keys();
+
+		while (keys.hasMoreElements()) {
+			Object key = keys.nextElement();
+			if (key.equals("filtroBebidas")) {
+				res=propiedadesEspanol.get("filtroBebidas").toString();
+			}
+		}
+		System.out.println("res: "+res);
+		assertEquals(res, "");
+	}
+	
 }
--- sugerenciasExternasPP2/sugerenciasExternasPP2.Principal/src/test/java/iteracion2_CriteriosDeAceptaion/Us4_It2Test.java
+++ sugerenciasExternasPP2/sugerenciasExternasPP2.Principal/src/test/java/iteracion2_CriteriosDeAceptaion/Us4_It2Test.java
@@ -1,5 +1,71 @@
 package iteracion2_CriteriosDeAceptaion;
 
-public class Us4_It2Test {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import validaciones.ValidarEmail;
+import validaciones.ValidarUsuario;
+import bo.UsuariosBo;
+
+public class Us4_It2Test {
+	UsuariosBo uBo;
+	ValidarEmail ve;
+	ValidarUsuario vu;
+	
+	@Before
+	public void init(){
+		uBo= new UsuariosBo();
+		ve= new ValidarEmail();
+		vu= new ValidarUsuario();
+	}
+	//Si se ingresa un usuario correcto “federico” , entonces el formatos es correcto, devuelve “true”
+	@Test
+	public void test1(){
+		System.out.println("Test 1!");
+		boolean res=false;
+		res= uBo.caracteresValidosUsuario("federico");
+		
+		assertTrue(res);
+	}
+	//Si se ingresa un usuario incorrecto “die” , entonces no cumple con un formato de mínimo 5 caracteres. Devuelve “false”.
+	@Test
+	public void test2(){
+		System.out.println("Test 2!");
+		boolean res=true;
+		res= uBo.caracteresValidosUsuario("die");
+		
+		assertFalse(res);
+	}
+	//Si se ingresa un email correcto federico@yahoo.com.ar , entonces al cumplir con el formato, devuelve “true”
+	@Test
+	public void test3(){
+		System.out.println("Test 3!");
+		boolean res=false;
+		res= ve.isEmail("federico@yahoo.com.ar");
+		
+		assertTrue(res);
+	}
+	//Si se ingresa un email incorrecto federico.yahoo.com.ar, entonces al no cumplir con el formato de que debe de haber un @ devuelve “false”.
+	@Test
+	public void test4(){
+		System.out.println("Test 4!");
+		boolean res=true;
+		res= ve.isEmail("federico.yahoo.com.ar");
+		
+		assertFalse(res);
+	}
+	//Si se ingresa un usuario ya existente, no lo guarda
+	@Test
+	public void test5(){
+		System.out.println("Test 5!");
+		boolean res=false;
+		res= vu.elUsuarioYaExiste("fernando", "fernando@yahoo.com.ar");
+		
+		assertTrue(res);
+	}
+	
 }
