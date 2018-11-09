0c9de760a683a6fd26583faced38dbce8d408bda
fjgalvan <fjgalvanx87@gmail.com>
2018-11-09 19:21:05 -0300

    jacoco cobertura

--- sugerenciasExternasPP2/sugerenciasExternasPP2.Principal/src/test/java/dto/CustomerResourceTest.java
+++ sugerenciasExternasPP2/sugerenciasExternasPP2.Principal/src/test/java/dto/CustomerResourceTest.java
@@ -1,62 +1,62 @@
 package dto;
 
 
-
-
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Test;
-
-/**
- * tests {@link CustomerResource}.
- */
-public class CustomerResourceTest {
-//  @Test
-//  public void shouldGetAllCustomersTest() {
-//    CustomerDto customer = new CustomerDto("1", "usuarioA", "chatarras", "postres");
-//    List<CustomerDto> customers = new ArrayList<>();
-//    customers.add(customer);
 //
-//    CustomerResource customerResource = new CustomerResource(customers);
 //
-//    List<CustomerDto> allCustomers = customerResource.getAllCustomers();
+//import static org.junit.Assert.*;
 //
-//    assertEquals(allCustomers.size(), 1);
-//    assertEquals(allCustomers.get(0).getId(), "1");
-//    assertEquals(allCustomers.get(0).getUserName(), "usuarioA");
-//    assertEquals(allCustomers.get(0).getTipoComida1(), "chatarras");
-//    assertEquals(allCustomers.get(0).getTipoComida2(), "postres");
-//  }
+//import java.util.ArrayList;
+//import java.util.List;
 //
-//  @Test
-//  public void shouldSaveCustomerTest() {
-//    CustomerDto customer = new CustomerDto("1", "usuarioA", "chatarras", "postres");
-//    CustomerResource customerResource = new CustomerResource(new ArrayList<>());
+//import org.junit.Test;
 //
-//    customerResource.save(customer);
+///**
+// * tests {@link CustomerResource}.
+// */
+//public class CustomerResourceTest {
+////  @Test
+////  public void shouldGetAllCustomersTest() {
+////    CustomerDto customer = new CustomerDto("1", "usuarioA", "chatarras", "postres");
+////    List<CustomerDto> customers = new ArrayList<>();
+////    customers.add(customer);
+////
+////    CustomerResource customerResource = new CustomerResource(customers);
+////
+////    List<CustomerDto> allCustomers = customerResource.getAllCustomers();
+////
+////    assertEquals(allCustomers.size(), 1);
+////    assertEquals(allCustomers.get(0).getId(), "1");
+////    assertEquals(allCustomers.get(0).getUserName(), "usuarioA");
+////    assertEquals(allCustomers.get(0).getTipoComida1(), "chatarras");
+////    assertEquals(allCustomers.get(0).getTipoComida2(), "postres");
+////  }
+////
+////  @Test
+////  public void shouldSaveCustomerTest() {
+////    CustomerDto customer = new CustomerDto("1", "usuarioA", "chatarras", "postres");
+////    CustomerResource customerResource = new CustomerResource(new ArrayList<>());
+////
+////    customerResource.save(customer);
+////
+////    List<CustomerDto> allCustomers = customerResource.getAllCustomers();
+////    assertEquals(allCustomers.get(0).getId(), "1");
+////    assertEquals(allCustomers.get(0).getUserName(), "usuarioA");
+////    assertEquals(allCustomers.get(0).getTipoComida1(), "chatarras");
+////    assertEquals(allCustomers.get(0).getTipoComida2(), "postres");
+////  }
+////
+////  @Test
+////  public void shouldDeleteCustomerTest() {
+////    CustomerDto customer = new CustomerDto("1", "usuarioA", "chatarras", "postres");
+////    List<CustomerDto> customers = new ArrayList<>();
+////    customers.add(customer);
+////
+////    CustomerResource customerResource = new CustomerResource(customers);
+////
+////    customerResource.delete(customer.getId());
+////
+////    List<CustomerDto> allCustomers = customerResource.getAllCustomers();
+////    assertEquals(allCustomers.size(), 0);
+////  }
 //
-//    List<CustomerDto> allCustomers = customerResource.getAllCustomers();
-//    assertEquals(allCustomers.get(0).getId(), "1");
-//    assertEquals(allCustomers.get(0).getUserName(), "usuarioA");
-//    assertEquals(allCustomers.get(0).getTipoComida1(), "chatarras");
-//    assertEquals(allCustomers.get(0).getTipoComida2(), "postres");
-//  }
-//
-//  @Test
-//  public void shouldDeleteCustomerTest() {
-//    CustomerDto customer = new CustomerDto("1", "usuarioA", "chatarras", "postres");
-//    List<CustomerDto> customers = new ArrayList<>();
-//    customers.add(customer);
-//
-//    CustomerResource customerResource = new CustomerResource(customers);
-//
-//    customerResource.delete(customer.getId());
-//
-//    List<CustomerDto> allCustomers = customerResource.getAllCustomers();
-//    assertEquals(allCustomers.size(), 0);
-//  }
-
-}
+//}
