526835b92c7661afeca896b5a961cf8accb2a15e
fjgalvan <fjgalvanx87@gmail.com>
2018-11-28 11:28:58 -0300

    test

--- sugerenciasExternasPP2/sugerenciasExternasPP2.Principal/src/test/java/estadisticas/EstadisticasTest.java
+++ sugerenciasExternasPP2/sugerenciasExternasPP2.Principal/src/test/java/estadisticas/EstadisticasTest.java
@@ -1,11 +1,19 @@
 package estadisticas;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 
+import modelo.Customer;
+import modelo.Preferencias;
+import modelo.Recomendacion;
+import modelo.Usuario;
 import mvc_modelo_observable.Modelo;
 
 import org.junit.Test;
 
+import com.mongodb.DBCollection;
+
+import conexiones.Interfaz.RecolectorPromos;
 import dao.mongoDB.MongoConcreteStub;
 import twitter4j.TwitterException;
 
@@ -16,19 +24,30 @@ public class EstadisticasTest {
 		Modelo m = new Modelo("recomendaciones","usuario", "email", "horarioActualizacion");
 		m.cargarCustomers();
 		m.cargarMapReco();
-		m.cargarRecomendacionesGenerales(m.cargarMapReco());
+		m.cargarRecomendacionesGenerales(m.getMapRecomendaciones());
 		m.cargarTodasLasPromos();
 		m.ConectarMongoDBStub();
-		MongoConcreteStub mongo= new MongoConcreteStub();
-		Modelo modelo= new Modelo("a","e","i","0");
-		modelo.ConectarMongoDBStub();
-		modelo.cargarRecomendacionesGenerales(modelo.getMapRecomendaciones());
-		mongo.setColl(modelo.cargarTodasLasPromos());
-		p.getPromoMasCara(m.getMongo().getPromos());
-		p.getPromoMasEconomica(m.getMongo().getPromos());
-		p.getPromoMasCara(modelo.cargarTodasLasPromos());
-		p.getPromoMasEconomica(modelo.cargarTodasLasPromos());
+		RecolectorPromos c = new RecolectorPromos();//sin argumento con mongoStub
+		
+		c.cargarListaConectores();
+		c.buscarPromociones();
+		c.promosConFiltros("chatarras", "postres");
+		//c.getMongoDB().leerColeccion();
+		Usuario u= new Usuario("u","usuario@yahoo.com.ar");
+		ArrayList<Preferencias> listaPreferencias= new ArrayList<Preferencias>();
+		Preferencias p1= new Preferencias(1,"chatarras");
+		Preferencias p2= new Preferencias(2,"postres");
+		listaPreferencias.add(p1);
+		listaPreferencias.add(p2);
+		Customer c1= new Customer("10", u,listaPreferencias);
+		Recomendacion reco= new Recomendacion(c1);
+		reco.leerPreferencias();
+		DBCollection coll=reco.mostrarRecomendaciones(c.getMongoDB().getPromos());
+		System.out.println("coll count: "+ coll.count());
+		p.getPromoMasCara(coll);
+		p.getPromoMasEconomica(coll);
 	}
+	
 	
 	
     
